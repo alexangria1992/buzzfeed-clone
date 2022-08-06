28a6f061eaf2844fa6ecef6f9e4a9cf15e9abe2a
alexangria1992 <alex040892@gmail.com>
2022-08-06 18:33:39 +1200

    Title and answer block initialisation

--- app.js
+++ app.js
@@ -105,3 +105,39 @@ const questions = [
     ],
   },
 ];
+
+const populateQuestions = () => {
+  questions.forEach((question) => {
+    //TITLE BLOCK
+    const titleBlock = document.createElement("div");
+    titleBlock.id = question.id;
+    titleBlock.classList.add("title-block");
+
+    const titleHeading = document.createElement("h2");
+    titleHeading.textContent = question.text;
+    titleBlock.append(titleHeading);
+    questionDisplay.append(titleBlock);
+
+    //ANSWERS BLOCK
+    const answersBlock = document.createElement("div");
+    answersBlock.id = question.id + "-questions";
+    answersBlock.classList.add("answer-options");
+
+    question.answers.forEach((answer) => {
+      const answerBlock = document.createElement("div");
+      answerBlock.classList.add("answer-block");
+      answerBlock.addEventListener("click", handleClick);
+
+      //ANSWER IMAGE
+      const answerImage = document.createElement("img");
+    });
+
+    questionDisplay.append(answersBlock);
+  });
+};
+
+populateQuestions();
+
+const handleClick = () => {
+  console.log("clicked");
+};
