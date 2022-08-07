c0fc73eedc71d53d91df01b54f61840e0e54f624
alexangria1992 <alex040892@gmail.com>
2022-08-07 14:03:37 +1200

    show answers

--- app.js
+++ app.js
@@ -9,28 +9,28 @@ const questions = [
       {
         text: "New York",
         image:
-          "https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
+          "https://images.unsplash.com/photo-1428366890462-dd4baecf492b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjAxNA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Photo of the Empire State building during daytime",
         credit: "Oliver Niblett",
       },
       {
         text: "Austin",
         image:
-          "https://images.unsplash.com/photo-1473042904451-00171c69419d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=899&q=80",
+          "https://images.unsplash.com/photo-1485445636332-cbe2d86716dd?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjMyMw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Time-lapse photography car light on bridge",
         credit: "Carlos Alfonso",
       },
       {
         text: "Portland",
         image:
-          "https://images.unsplash.com/photo-1472510771109-39b92752a6b9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          "https://images.unsplash.com/photo-1645934430496-6cae81215bf9?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjQ3MQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "High-rise buildings",
         credit: "Elena Kuchko ",
       },
       {
         text: "New Orleans",
         image:
-          "https://images.unsplash.com/photo-1558390640-383986a7cd2f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+          "https://images.unsplash.com/photo-1544078042-81e5cc35e72a?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjU2Mw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Road with people and house",
         credit: "Joao Francisco ",
       },
@@ -43,28 +43,28 @@ const questions = [
       {
         text: "Pizza",
         image:
-          "https://images.unsplash.com/photo-1571066811602-716837d681de?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=536&q=80",
+          "https://images.unsplash.com/photo-1571066811602-716837d681de?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjcyMQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Pepperoni Pizza",
         credit: "Alan Hardman",
       },
       {
         text: "Sandwich",
         image:
-          "https://images.unsplash.com/photo-1559054663-e8d23213f55c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "https://images.unsplash.com/photo-1559054663-e8d23213f55c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjc3OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Ham sandwich on white surface",
         credit: "Eaters Collective",
       },
       {
         text: "Pasta",
         image:
-          "https://images.unsplash.com/photo-1546549032-9571cd6b27df?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "https://images.unsplash.com/photo-1546549032-9571cd6b27df?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjg3Mw&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Pasta in tomato sauce",
         credit: "Mgg Vitchakorn",
       },
       {
         text: "Hamburger",
         image:
-          "https://images.unsplash.com/photo-1561758033-d89a9ad46330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          "https://images.unsplash.com/photo-1550317138-10000687a72b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMjkwOQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "Hamburger",
         credit: "sk",
       },
@@ -77,34 +77,75 @@ const questions = [
       {
         text: "traditional",
         image:
-          "https://images.unsplash.com/photo-1627553524956-311288fed658?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
+          "https://images.unsplash.com/photo-1648822502458-9cfddddb7a95?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMzE4OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "focus photography of building windows",
         credit: "Burgess Milner",
       },
       {
         text: "Modern",
         image:
-          "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+          "https://images.unsplash.com/photo-1600585154340-be6161a56a0c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMzM1MQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "low angle view of building",
         credit: "Brandon Giggs",
       },
       {
         text: "House",
         image:
-          "https://images.unsplash.com/photo-1593604340846-4fbe9763a8f3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+          "https://images.unsplash.com/photo-1531663766899-27c5b930c76b?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMzUwMA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "trees beside white house",
         credit: "Phil Hearing",
       },
       {
         text: "Mountains",
         image:
-          "https://images.unsplash.com/photo-1485160497022-3e09382fb310?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+          "https://images.unsplash.com/photo-1506905925346-21bda4d32df4?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=200&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzMzM5MQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
         alt: "brown wooden cabin in front of forest",
         credit: "eulauretta",
       },
     ],
   },
 ];
+
+const answers = [
+  {
+    combination: ["New York", "Pizza", "Traditional"],
+    text: "Blue Cheese",
+    image:
+      "https://images.unsplash.com/photo-1626957341926-98752fc2ba90?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=230&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzNTc3OQ&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
+    alt: "Blue Cheese",
+  },
+  {
+    combination: ["Austin", "Pasta", "Modern"],
+    text: "Cheddar",
+    // image: "https://images.unsplash.com/photo-1618164436241-4473940d1f5c?crop=entropy&cs=tinysrgb&fit=crop&fm=jpg&h=230&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY1OTgzNjA5OA&ixlib=rb-1.2.1&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=300",
+    image: "",
+    alt: "Cheddar Cheese",
+  },
+  {
+    combination: ["Portland", "Sandwich", "Mountains"],
+    text: "Feta",
+    image: "",
+    alt: "Feta Cheese",
+  },
+  {
+    combination: ["Portland", "Sandwich", "Mountains"],
+    text: "Feta",
+    image: "",
+    alt: "Feta",
+  },
+  {
+    combination: ["New Orleans", "Hamburger", "House"],
+    text: "Halloumi",
+    image: "",
+    alt: "Halloumi",
+  },
+];
+
+//Need to have a default answer to compensate for lack of combination data
+
+const unansweredQuestions = [];
+
+const chosenAnswers = [];
 
 const populateQuestions = () => {
   questions.forEach((question) => {
@@ -123,10 +164,14 @@ const populateQuestions = () => {
     answersBlock.id = question.id + "-questions";
     answersBlock.classList.add("answer-options");
 
+    unansweredQuestions.push(question.id);
+
     question.answers.forEach((answer) => {
       const answerBlock = document.createElement("div");
       answerBlock.classList.add("answer-block");
-      answerBlock.addEventListener("click", () => handleClick);
+      answerBlock.addEventListener("click", () =>
+        handleClick(question.id, answer.text)
+      );
 
       //ANSWER IMAGE
       const answerImage = document.createElement("img");
@@ -140,10 +185,11 @@ const populateQuestions = () => {
       const answerInfo = document.createElement("p");
       const imageLink = document.createElement("a");
       imageLink.setAttribute("href", answer.credit);
+      imageLink.textContent = answer.credit;
       const sourceLink = document.createElement("a");
       sourceLink.textContent = "Unsplash";
-      sourceLink.setAttribute("src", "https://www.unsplash.com");
-      answerInfo.append(imageLink, "to", sourceLink);
+      sourceLink.setAttribute("src", " https://www.unsplash.com");
+      answerInfo.append(imageLink, " to ", sourceLink);
 
       answerBlock.append(answerImage, answerTitle, answerInfo);
       answersBlock.append(answerBlock);
@@ -155,6 +201,55 @@ const populateQuestions = () => {
 
 populateQuestions();
 
-const handleClick = () => {
-  console.log("clicked");
+const handleClick = (questionId, chosenAnswer) => {
+  //   console.log(questionId, chosenAnswer);
+  if (unansweredQuestions.includes(questionId))
+    chosenAnswers.push(chosenAnswer);
+  const itemToRemove = unansweredQuestions.indexOf(questionId);
+
+  if (itemToRemove > -1) {
+    unansweredQuestions.splice(itemToRemove, 1);
+  }
+
+  console.log(chosenAnswers);
+  console.log(unansweredQuestions);
+
+  //   disableQuestionBlock(questionId, chosenAnswer)
+  const lowestQuestionId = Math.min(...unansweredQuestions);
+  location.href = "#" + lowestQuestionId;
+
+  //scroll to top most unanswered question
+  if (!unansweredQuestions.length) {
+    //Scroll to answer div
+    showAnswer();
+  }
+};
+
+const showAnswer = () => {
+  let result;
+  answers.forEach((answer) => {
+    if (
+      chosenAnswers.includes(answer.combination[0]) +
+      chosenAnswers.includes(answer.combination[1]) +
+      chosenAnswers.includes(answer.combination[2])
+    ) {
+      result = answer;
+    } else {
+      // first answer object is default
+      result = answers[0];
+    }
+  });
+  console.log(result);
+
+  const answerBlock = document.createElement("div");
+  answerBlock.classList.add("result-block");
+  const answerTitle = document.createElement("h3");
+  answerTitle.textContent = result.text;
+  const answerImage = document.createElement("img");
+  answerImage.setAttribute("src", result.image);
+  answerImage.setAttribute("alt", result.alt);
+
+  answerBlock.append(answerTitle, answerImage);
+
+  answerDisplay.append(answerBlock);
 };
